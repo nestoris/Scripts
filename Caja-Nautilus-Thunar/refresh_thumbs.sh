c354756d5add5d4060ba35de4c4ef555a85ff84d
Evgeny Dobromilsky <voltasar@gmail.com>
2021-06-20 11:33:49 +0300

    make it compatible with Thunar

--- Caja-Nautilus-Thunar/refresh_thumbs.sh
+++ Caja-Nautilus-Thunar/refresh_thumbs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for i in $NAUTILUS_SCRIPT_SELECTED_FILE_PATHS; do
-rm `gio info "$i"|awk '/thumbnail::path/{sub(/^.* /,"",$0);print}'`
+for i in "$@"; do
+rm $(gio info "$i"|awk '/thumbnail::path/{sub(/^.* /,"",$0);print}')
 done
