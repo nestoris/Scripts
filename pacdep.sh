deecf0a3c9bf620220942e45989d81237e7373c0
Evgeny Dobromilsky <voltasar@gmail.com>
2021-07-20 18:58:30 +0300

    Update pacdep.sh

--- pacdep.sh
+++ pacdep.sh
@@ -5,5 +5,5 @@ let x+=1
 [[ $x == 1 ]] && echo "Packages dependent on..."
 echo -e "\e[1m$i:\e[0m"
 paclist=$(LC_ALL=C pacman -Qi "$i" 2>/dev/null|gawk -F" : " '/.equired/{if($2!="None"){print $2}}')
-[[ ! -z $paclist ]] && LC_ALL=C pacman -Qi $paclist|awk -F": " '/^Name/{n="\033[1;32m "$2"\033[0m";next}/^Description/{print n " \033[3m" $2"\033[0m"}'
+[[ ! -z $paclist ]] && LC_ALL=C pacman -Qi $paclist|gawk -F": " '/^Name/{n="\033[1;32m "$2"\033[0m";next}/^Description/{print n " \033[3m" $2"\033[0m"}'
 done
